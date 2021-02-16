e230d796c7d7c0f591eaf1920ab61acb1711bb8e
ernestas_raudonis <raudonisernestas@gmail.com>
2021-02-16 13:17:04 +0200

    Create a sign up screen.

--- features/login/src/main/java/com/raudonikis/login/login/LoginFragment.kt
+++ features/login/src/main/java/com/raudonikis/login/login/LoginFragment.kt
@@ -1,5 +1,6 @@
-package com.raudonikis.login
+package com.raudonikis.login.login
 
 import androidx.fragment.app.Fragment
+import com.raudonikis.login.R
 
 class LoginFragment : Fragment(R.layout.fragment_login)
---
+++ features/login/src/main/java/com/raudonikis/login/signup/SignUpFragment.kt
@@ -0,0 +1,6 @@
+package com.raudonikis.login.signup
+
+import androidx.fragment.app.Fragment
+import com.raudonikis.login.R
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up)
