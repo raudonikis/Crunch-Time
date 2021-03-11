77843d2ce6d6247a7f30750df4accbd4d1020b9f
ernestas_raudonis <raudonisernestas@gmail.com>
2021-03-11 13:57:37 +0200

    Set up profile fragment

--- features/profile/src/main/java/com/raudonikis/profile/ProfileFragment.kt
+++ features/profile/src/main/java/com/raudonikis/profile/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.raudonikis.profile
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.raudonikis.profile.databinding.FragmentProfileBinding
@@ -11,4 +13,26 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private val viewModel: ProfileViewModel by viewModels()
     private val binding: FragmentProfileBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpListeners()
+    }
+
+    /**
+     * Listeners
+     */
+    private fun setUpListeners() {
+        binding.apply {
+            cardPlayed.setOnClickListener {
+
+            }
+            cardPlaying.setOnClickListener {
+
+            }
+            cardWant.setOnClickListener {
+
+            }
+        }
+    }
 }
