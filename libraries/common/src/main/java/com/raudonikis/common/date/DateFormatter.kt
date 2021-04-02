3169bddacdabfb7ded3bd708f8f723aee734f1c4
ernestas_raudonis <raudonisernestas@gmail.com>
2021-04-02 13:05:11 +0300

    Date formatter - parse the hours/minutes/seconds as well.

--- libraries/common/src/main/java/com/raudonikis/common/date/DateFormatter.kt
+++ libraries/common/src/main/java/com/raudonikis/common/date/DateFormatter.kt
@@ -1,19 +1,23 @@
 package com.raudonikis.common.date
 
 import timber.log.Timber
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 object DateFormatter {
 
-    private val formatter
+    //2021-02-28T20:06:55.000000Z
+    private val parser: DateFormat
+        get() = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+    private val formatter: DateFormat
         get() = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
 
     fun stringToDate(dateString: String): Date? {
         return try {
-            formatter.parse(dateString)
+            parser.parse(dateString)
         } catch (e: Exception) {
-            Timber.w("Could not parse date -> $dateString")
+            Timber.w("Could not parse date -> $dateString, exception -> ${e.message}")
             null
         }
     }
@@ -22,7 +26,7 @@ object DateFormatter {
         return try {
             formatter.format(date)
         } catch (e: Exception) {
-            Timber.w("Could not format date -> $date")
+            Timber.w("Could not format date -> $date, exception -> ${e.message}")
             ""
         }
     }
