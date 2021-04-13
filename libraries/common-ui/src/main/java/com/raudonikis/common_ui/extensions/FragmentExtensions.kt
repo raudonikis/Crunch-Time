66bf4263434a94da84bfe7cce50f0f86645312a8
ernestas_raudonis <raudonisernestas@gmail.com>
2021-04-13 21:34:24 +0300

    Fix hideKeyboard extension

--- libraries/common-ui/src/main/java/com/raudonikis/common_ui/extensions/FragmentExtensions.kt
+++ libraries/common-ui/src/main/java/com/raudonikis/common_ui/extensions/FragmentExtensions.kt
@@ -1,19 +1,16 @@
 package com.raudonikis.common_ui.extensions
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 
 fun Fragment.hideKeyboard() {
-    getSystemService(requireContext(), InputMethodManager::class.java)?.toggleSoftInput(
-        InputMethodManager.SHOW_IMPLICIT,
-        0
-    )
+    view?.let { activity?.hideKeyboard(it) }
 }
 
-fun Fragment.showKeyboard() {
-    getSystemService(requireContext(), InputMethodManager::class.java)?.toggleSoftInput(
-        InputMethodManager.HIDE_IMPLICIT_ONLY,
-        0
-    )
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
