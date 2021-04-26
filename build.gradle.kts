e9bc17a8035dd099aaeb716dd7fec8e67083914c
ernestas_raudonis <raudonisernestas@gmail.com>
2021-04-26 14:38:29 +0300

    Change build.gradle.kts versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ buildscript {
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
         // Hilt
-        classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.33-beta")
         // Navigation
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
