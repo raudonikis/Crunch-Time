cfd13ff87087b46a49cb65f34ffae309696ca16f
ernestas_raudonis <raudonisernestas@gmail.com>
2021-04-26 15:32:11 +0300

    Fix lint errors and disable lint error abortion

---
+++ libraries/data-domain/src/test/java/com/raudonikis/data_domain/activity/usecases/MyActivityUseCaseTest.kt
@@ -0,0 +1,19 @@
+package com.raudonikis.data_domain.activity.usecases
+
+import org.junit.Before
+import org.junit.Test
+
+class MyActivityUseCaseTest {
+
+    private lateinit var myActivityUseCase: MyActivityUseCase
+
+    @Before
+    fun setup() {
+//        myActivityUseCase = MyActivityUseCase()
+    }
+
+    @Test
+    fun `idk`() {
+
+    }
+}
