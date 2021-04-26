f9f9a388e64a716580ed16e76106281623efa00f
ernestas_raudonis <raudonisernestas@gmail.com>
2021-04-26 16:57:33 +0300

    Test CI/CD

--- libraries/data-domain/src/test/java/com/raudonikis/data_domain/activity/usecases/MyActivityUseCaseTest.kt
+++ libraries/data-domain/src/test/java/com/raudonikis/data_domain/activity/usecases/MyActivityUseCaseTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class MyActivityUseCaseTest {
 
     private lateinit var myActivityUseCase: MyActivityUseCase
-    private var i = 2
+    private var i = 1
 
     @Before
     fun setup() {
