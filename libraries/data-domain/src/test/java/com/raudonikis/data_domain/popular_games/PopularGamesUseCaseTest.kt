3561ff8cebad5b881cd872adea7e003adb83f50b
ernestas_raudonis <raudonisernestas@gmail.com>
2021-04-27 17:23:02 +0300

    Add unit tests for PopularGamesUseCase.kt

--- libraries/data-domain/src/test/java/com/raudonikis/data_domain/popular_games/PopularGamesUseCaseTest.kt
+++ libraries/data-domain/src/test/java/com/raudonikis/data_domain/popular_games/PopularGamesUseCaseTest.kt
@@ -1,7 +1,7 @@
-package com.raudonikis.data_domain.news_feed
+package com.raudonikis.data_domain.popular_games
 
 import com.raudonikis.common.Outcome
-import com.raudonikis.data_domain.activity.cache.UserActivityDao
+import com.raudonikis.data_domain.game.cache.GameDao
 import com.raudonikis.network.GamesApi
 import com.raudonikis.network.utils.NetworkResponse
 import io.mockk.*
@@ -16,23 +16,23 @@ import org.junit.Before
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
-class NewsFeedUseCaseTest {
+class PopularGamesUseCaseTest {
 
-    private lateinit var newsFeedUseCase: NewsFeedUseCase
+    private lateinit var popularGamesUseCase: PopularGamesUseCase
 
     @RelaxedMockK
     private lateinit var gamesApi: GamesApi
 
     @RelaxedMockK
-    private lateinit var userActivityDao: UserActivityDao
+    private lateinit var gameDao: GameDao
 
     private val testDispatcher = TestCoroutineDispatcher()
 
     @Before
     fun setup() {
         MockKAnnotations.init(this)
-        newsFeedUseCase =
-            NewsFeedUseCase(userActivityDao, gamesApi, testDispatcher)
+        popularGamesUseCase =
+            PopularGamesUseCase(gameDao, gamesApi, testDispatcher)
     }
 
     @After
@@ -41,53 +41,52 @@ class NewsFeedUseCaseTest {
     }
 
     @Test
-    fun `getNewsFeed, returns from UserActivityDao`() {
+    fun `getPopularGames, returns from gameDao`() {
         //Assemble
         val expectedResult = flowOf(Outcome.empty())
-        every { userActivityDao.getNewsFeed() } returns expectedResult
+        every { gameDao.getPopularGames() } returns expectedResult
         //Act
-        val result = newsFeedUseCase.getNewsFeed()
+        val result = popularGamesUseCase.getPopularGames()
         //Assert
         assertEquals(expectedResult, result)
     }
 
     @Test
-    fun `updateNewsFeed, when gamesApi throws exception, returns failure`() =
+    fun `updatePopularGames, when gamesApi throws exception, returns failure`() =
         runBlockingTest {
             //Assemble
-            coEvery { gamesApi.getNewsFeed() } throws Exception("error")
+            coEvery { gamesApi.getPopularGames() } throws Exception("error")
             val expectedResult = Outcome.failure(message = "error")
             //Act
-            val result = newsFeedUseCase.updateNewsFeed()
+            val result = popularGamesUseCase.updatePopularGames()
             //Assert
             assertEquals(expectedResult, result)
         }
 
     @Test
-    fun `updateNewsFeed, calls getNewsFeed from gamesApi`() =
+    fun `updatePopularGames, calls getPopularGames from gamesApi`() =
         runBlockingTest {
             //Assemble
             //Act
-            newsFeedUseCase.updateNewsFeed()
+            popularGamesUseCase.updatePopularGames()
             //Assert
-            coVerify { gamesApi.getNewsFeed() }
+            coVerify { gamesApi.getPopularGames() }
         }
 
     @Test
-    fun `updateNewsFeed, sets Loading state, then sets Result state`() =
+    fun `updatePopularGames, sets Loading state, then sets Result state`() =
         runBlockingTest {
             //Assemble
-            coEvery { gamesApi.getNewsFeed() } returns NetworkResponse(
+            coEvery { gamesApi.getPopularGames() } returns NetworkResponse(
                 isSuccess = true,
                 data = listOf())
             val expectedResult = Outcome.empty()
             //Act
-            newsFeedUseCase.updateNewsFeed()
+            popularGamesUseCase.updatePopularGames()
             //Assert
             verifySequence {
-                userActivityDao.setNewsFeedOutcome(Outcome.loading())
-                userActivityDao.setNewsFeedOutcome(expectedResult)
+                gameDao.setPopularGamesOutcome(Outcome.loading())
+                gameDao.setPopularGamesOutcome(expectedResult)
             }
         }
-
 }
