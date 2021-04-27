f61d0b8dd99b4e9bea94a5fcb1938e747156b499
ernestas_raudonis <raudonisernestas@gmail.com>
2021-04-27 15:02:14 +0300

    Add tests for MyActivityUseCase.kt

--- libraries/data-domain/src/main/java/com/raudonikis/data_domain/activity/usecases/MyActivityUseCase.kt
+++ libraries/data-domain/src/main/java/com/raudonikis/data_domain/activity/usecases/MyActivityUseCase.kt
@@ -6,6 +6,7 @@ import com.raudonikis.data_domain.activity.mappers.UserActivityMapper
 import com.raudonikis.data_domain.activity.models.UserActivity
 import com.raudonikis.network.GamesApi
 import com.raudonikis.network.utils.safeNetworkResponse
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
@@ -14,11 +15,12 @@ import javax.inject.Inject
 class MyActivityUseCase @Inject constructor(
     private val userActivityDao: UserActivityDao,
     private val gamesApi: GamesApi,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
     fun getMyUserActivity(): Flow<Outcome<List<UserActivity>>> = userActivityDao.getMyUserActivity()
 
     suspend fun updateMyUserActivity(): Outcome<List<UserActivity>> {
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             safeNetworkResponse {
                 gamesApi.getUserActivity()
                     .map {
