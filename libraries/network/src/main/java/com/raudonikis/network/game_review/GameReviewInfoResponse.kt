c47b526c852c726aa61451c3e8c413fc1dd69179
raudonikis <raudonisernestas@gmail.com>
2021-05-18 21:04:20 +0300

    Fix response model json name

--- libraries/network/src/main/java/com/raudonikis/network/game_review/GameReviewInfoResponse.kt
+++ libraries/network/src/main/java/com/raudonikis/network/game_review/GameReviewInfoResponse.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 data class GameReviewInfoResponse(
     @Json(name = "positiveCount")
     val positiveCount: Int,
-    @Json(name = "negativeCounte")
+    @Json(name = "negativeCount")
     val negativeCount: Int,
     @Json(name = "reviews")
     val reviews: List<GameReviewResponse> = listOf(),
